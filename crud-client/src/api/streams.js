65a773a93e1f63bc06f845fc852282b12657087d
WebDev <39054829+WebDevelopUa@users.noreply.github.com>
2020-07-05 11:36:44 +0300

    Update streams.js

--- crud-client/src/api/streams.js
+++ crud-client/src/api/streams.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 // const development = 'http://localhost:3001/';
-const production = 'https://crud-json-server.herokuapp.com/';
+const production = 'https://stream-json-server.herokuapp.com/';
 
 export default axios.create({
     baseURL: production
